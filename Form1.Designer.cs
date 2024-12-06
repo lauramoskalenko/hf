9468d73b8e9de20f4e26dd9666dfce23ef04211e
lauramoskalenko <moskalenkolaura@gmail.com>
2024-12-06 21:25:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace HelloWorldApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показати текст";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 250);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hello World App";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+    }
+}
